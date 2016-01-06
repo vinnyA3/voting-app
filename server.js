ad0413267d3b7b12ce28d437f1e0f6232c0f4cef
vinnya3 <vinaceto@yahoo.com>
2016-01-06 00:44:11 -0500

    added proper configuration

--- server.js
+++ server.js
@@ -1,25 +1,60 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
     morgan = require('morgan'),
     path = require('path'),
+    passport = require('passport'),
+    mongoose = require('mongoose'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    configDB = require('./config/database'),
     port = process.env.PORT || 8080;
 
-//bpdy parser - allow use to handle posts
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+//CONFIGURATION ======================================================
+mongoose.connect(configDB.db);
+
+require('./config/passport')(passport); //pass passport for configuration
+
 
 //handle cors request
 //app.use(function(req,res,next){ });
 
 //morgan - allow us to view allrequests in the console
 app.use(morgan('dev'));
+app.use(cookieParser());// read cookies - need for auth
+//body parser - allow use to handle posts
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//configuration to handle CORS requests
+app.use(function(req,res,next){
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, \ Authorization');
+    
+   next();
+});
+
 
 //set location for static files
 app.use(express.static(__dirname + '/public'));
 
-//get route
-app.get('/', function(req,res){
+//required for passport
+app.use(session({
+    secret:'theregoesamanwhooncehaditall',
+    resave: true,
+    saveUninitialized: true
+}));//secret key
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
+require('./app/routes/routes.js')(app,passport);
+
+//catch all route
+//send the users to the front end
+//has to be registered for the api routes
+app.get('*', function(req,res){
    res.sendFile(path.join(__dirname + '/public/app/views/index.html')); 
 });
 
