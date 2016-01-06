0a4070c4913e9b7b999e6f34b65b5f0649040d76
vinnya3 <vinaceto@yahoo.com>
2016-01-06 04:15:30 -0500

    fixed redirect bug

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,45 +1,52 @@
 module.exports = function(app,passport){
     
-    //process the signup form
-    //the credentials are processed through passport
-    //if we have a sucessful authentication, callback gets fired up and returns json with credentials
-    //else if authentication failed, then we respond with a 401 error ---
+    //process the signup form and run passport authentication
      app.post('/signup', function(req,res,next){
          passport.authenticate('local-signup', function(err,user,info){
             if(err){
-                console.log("We have an internal error");
                 return next(err); //will generate internal 500 error
             } 
              //generate a json response reflecting authentication status
              if(!user){
                  return res.send({success : false, message: 'authentication has failed!'});
              }
-             return res.send({success:true, message:'authentication success!'});
+             //explicitly run passport's login function
+             req.login(user, function(err){
+                if(err){return next(err);}
+                return res.send({success:true,message:'authentication has succeeded!'});
+             });
+    
          })(req,res,next);
      }); 
     
+    //handle login request and authenticate user
     app.post('/login', function(req,res,next){
         passport.authenticate('local-login', function(err,user,info){
            if(err){
-               console.log("we have an internal error!");
                return next(err);
            }
            if(!user){
                return res.send({success:false, message:'failed to login!'});
            }else{
-               console.log(req.user);
-               return res.send({success:true, message:'Logged in successfully!'});
+               //explicitly call passport's login function
+               req.login(user, function(err){
+                   if(err){
+                      return next(err);
+                    }
+                      return res.send({ success : true, message : 'authentication succeeded' }); 
+               });
            }
            
         })(req,res,next);
-    }); //handle when passport is configured
+    }); 
     
     //================================================
     //Protected api routes section
     //================================================
     
     app.get('/dashboard', isLoggedIn, function(req,res){
-        res.send({success: true, message:'logged in successfully!, this is an authenticated route!', user: req.user});
+        console.log('we hit the route!');
+        return res.send({success: true, message:'Hello, ' + req.user.local.name + '!'});
     });
     
     
@@ -47,7 +54,7 @@ module.exports = function(app,passport){
     // LOGOUT
     //================================================
     app.get('/logout', function(req,res){
-       req.logout();
+       req.logout();//passports logout function
        res.send({success:true, message:'Logged out successfully!'});
     });
     
@@ -59,9 +66,8 @@ function isLoggedIn(req,res,next){
     
     //if the user is authenticated in the session, carry on
     if(req.isAuthenticated()){
-        next();
+        return next();
     }
-    
     //if they are not authenticated in the session, redirect them to the home page
-    res.redirect('/');
+     res.redirect('/');
 }
