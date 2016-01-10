7977b72ce5ff138db5fe924208eac9cdb04fb934
vinnya3 <vinaceto@yahoo.com>
2016-01-10 03:06:33 -0500

    added signup controller

---
+++ public/app/controllers/signupCtrl.js
@@ -0,0 +1,24 @@
+angular.module('signupCtrl', ['authService'])
+    .controller('signupController', function($location, Auth){
+        var vm = this;
+    
+        //set error var
+        vm.error = false;
+    
+        //signup function
+        vm.signup = function(){
+            
+            Auth.signup(vm.signupData.name, vm.signupData.email, vm.signupData.password)
+                // on success
+                .then(function(data){
+                    $location.path('/dashboard');
+                    vm.signupData = {};
+                })
+                //handle the error
+                .catch(function(){
+                    vm.errorMessage = "Failed to signup. Please try again.";
+                    vm.error = true;
+                });
+        };//end up signup
+    
+    }); //end controller
