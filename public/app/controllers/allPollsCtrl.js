33957e5f37d8585e390e6eb11ce1f9fda403f273
vinnya3 <vinaceto@yahoo.com>
2016-01-13 12:03:31 -0500

    added processing variable

--- public/app/controllers/allPollsCtrl.js
+++ public/app/controllers/allPollsCtrl.js
@@ -3,12 +3,14 @@ angular.module('allPollsCtrl', ['pollsService'])
         
         var vm = this;
         vm.error = false;
+        vm.processing = true;
         
         vm.getAllPolls = function(){
             //get all polls
             Poll.getAllPolls()
                 .then(function(data){
-                     vm.allPolls = data;   
+                     vm.processing = false;
+                     vm.allPolls = data;
                 })
                 .catch(function(){
                     vm.error = true;
